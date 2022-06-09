18a0d9603b3603ea1eb4f755b3d8c8b31ec1503c
ankeett <ankeettkafle29@gmail.com>
2022-06-09 23:47:50 +0000

    some changes

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -13,12 +13,22 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    String name = request.getParameter("name");
+    String email = request.getParameter("email");
+    String subject = request.getParameter("subject");
+    String message = request.getParameter("message");
+
 
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println("Name: " + name);
+    System.out.println("\nEmail: " + email);
+    System.out.println("\nSubject: " + subject);
+    System.out.println("\nmessage: " + message);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    response.getWriter().println("\nName: " + name);
+    response.getWriter().println("\nEmail: " + email);
+    response.getWriter().println("\nSubject: " + subject);
+    response.getWriter().println("\nmessage: " + message);
   }
 }
